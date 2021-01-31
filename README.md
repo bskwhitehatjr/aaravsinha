bf6fad5f379e9ba36eb8be18f674c4b8a8247fd6
bskwhitehatjr <72731232+bskwhitehatjr@users.noreply.github.com>
2021-01-31 11:23:03 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# aaravsinha
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,15 @@
+function preload()
+{
+ track = loadImage("track.png");
+}
+
+function setup()
+ {
+  createCanvas(800,800);
+
+}
+
+function draw() 
+{
+background(track);
+}
